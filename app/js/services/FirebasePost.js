8df26b87852bc3b7f6357e98e95c6604686772ed
George <gkatsaros16@gmail.com>
2017-08-04 17:35:50 -0400

    posting to firebase works better now.

--- app/js/services/FirebasePost.js
+++ app/js/services/FirebasePost.js
@@ -5,18 +5,18 @@ tihcwlApp.factory('firebasePost', function(){
     writeWishList: function(bandArray) {
       //TODO: userId from facebook.
       //let userId = userId
-      database.ref('wishLists/').set({
+      database.ref('wishLists/126').update(
         // id
-        '131': bandArray.map(band => ({ name: band.name}))
-      })
-    },
-
-    updateUpcomingShowToPastShow: function(show) {
-      var stripedDate = show.date.replace(/\//g, '')
-
-      database.ref('shows/' + stripedDate).update({
-        upcomingShow: false
-      });
+        bandArray.map(band => ({ name: band.name}))
+      )
     }
+
+    // updateUpcomingShowToPastShow: function(show) {
+    //   var stripedDate = show.date.replace(/\//g, '')
+    //
+    //   database.ref('shows/' + stripedDate).update({
+    //     upcomingShow: false
+    //   });
+    // }
   }
 })
